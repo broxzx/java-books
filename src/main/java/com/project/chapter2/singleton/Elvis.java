d191e331c0e617e1724b154595d278e20e49d391
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-07-18 16:57:21 +0200

    singleton

---
+++ src/main/java/com/project/chapter2/singleton/Elvis.java
@@ -0,0 +1,15 @@
+package com.project.chapter2.singleton;
+
+public class Elvis {
+
+    private static final Elvis INSTANCE = new Elvis();
+
+    private Elvis() {
+
+    }
+
+    public static Elvis getInstance() {
+        return INSTANCE;
+    }
+
+}
