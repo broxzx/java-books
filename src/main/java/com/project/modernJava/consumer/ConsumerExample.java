f805178f25d8747f9e5e47dd0bf23e615d9e7443
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-08-05 12:59:23 +0200

    Consumer Example

---
+++ src/main/java/com/project/modernJava/consumer/ConsumerExample.java
@@ -0,0 +1,26 @@
+package com.project.modernJava.consumer;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        List<StringBuilder> strings = List.of(new StringBuilder("Random"), new StringBuilder("List"),
+                new StringBuilder("Name"), new StringBuilder("Java"), new StringBuilder("Effective"), new StringBuilder("In"), new StringBuilder("Action"));
+
+        Consumer<StringBuilder> consumer = (o1) -> {
+            o1.append(" changed");
+        };
+
+        consume(strings, consumer);
+        strings
+                .forEach(System.out::println);
+    }
+
+    private static void consume(List<StringBuilder> strings, Consumer<StringBuilder> consumer) {
+        strings
+                .forEach(consumer);
+    }
+
+}
